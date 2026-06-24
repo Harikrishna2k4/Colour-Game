57a3eab2509050b9ede4824c07411777e37d6a66
HARI KRISHNA R <ravchandiren196518@gmail.com>
2026-06-25 00:08:12 +0530

    vite changes

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin() // Forces CSS to load instantly with your JavaScript
+  ],
+  base: '/'
+})
+
